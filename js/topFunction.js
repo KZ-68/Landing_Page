7ea3dbe22102aa765a73cdcdfe4f1791bdb9ece6
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-11-18 19:15:12 +0100

    style: backtotop function

---
+++ js/topFunction.js
@@ -0,0 +1,22 @@
+let topBtn = document.querySelector("#scrollToTop");
+
+if(document.body.scrollTop == 0) {
+    topBtn.style.display = "none";
+} 
+
+window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+          topBtn.style.display = "block";
+        } else {
+          topBtn.style.display = "none";
+        }
+      }
+
+function topFunction() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    }); // Chrome, Firefox, IE et Opera
+}
